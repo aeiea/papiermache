f5024b2491b7970f543d270c2e0bb9908032d974
aeiea <118844761+aeiea@users.noreply.github.com>
2023-04-02 11:20:46 -0700

    init

---
+++ papiermache.py
@@ -0,0 +1,26 @@
+'''                                                                                           
+  _                                         
+ |_) _. ._  o  _  ._ __ |\/|  _.  _ |_   _  
+ |  (_| |_) | (/_ |     |  | (_| (_ | | (/_ 
+        |                                                                                                       
+        P A P I E R M A C H E v0.1a 
+    
+    Papiermache is a simple, lightweight, a
+-nd easy to use clone of the popular pasteb
+-in service, pastebin.com. It is written in 
+Python and uses the Flask microframework.
+
+                 📰FEATURES             
+- Simple UI(There is none) 
+- Easy to use(Doesn't exist) 
+- Environmental Friendly(Runs on potato)
+- Fast(Not even close)
+- Great dev team(1 hobo and a dog)
+- Free(You can't pay for it)
+- Open Source(You can't pay for it)
+- No ads(Don't know how to add them)
+- Lightweight(2.4 GB of dependencies)
+'''
+# Imports
+import flask
+from bleach import clean
